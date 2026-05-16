875938812fe9955c95ba8aae363a63ee30b90306
anisbharadwaj <anishbharadwaj685@gmail.com>
2026-05-16 17:15:05 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
-from app import app
+import sys
+import os
 
-app=app
+# This tells Vercel to look in the current folder for your app file
+sys.path.append(os.path.dirname(__file__))
+
+from app import app as application
+
+app = application
