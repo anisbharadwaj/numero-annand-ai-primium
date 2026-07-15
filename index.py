3f8e9e95d785218960d57bf2a18d4c5c3845cd9b
anisbharadwaj <anishbharadwaj685@gmail.com>
2026-07-15 08:24:27 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,3 @@
-import sys
-import os
+from app import create_app
 
-# This tells Vercel to look in the current folder for your app file
-sys.path.append(os.path.dirname(__file__))
-
-from app import app as application
-
-app = application
+app = create_app()
