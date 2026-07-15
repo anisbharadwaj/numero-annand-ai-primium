4f7e81a82bc4b85eacca5d12db267c3679737dd0
anisbharadwaj <anishbharadwaj685@gmail.com>
2026-07-15 08:26:31 +0530

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,9 +3,9 @@ from flask import Blueprint, render_template
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
-def index():
+def index_route():
     return render_template('index.html')
 
 @main_bp.route('/pricing')
-def pricing():
+def pricing_route():
     return render_template('pricing.html')
