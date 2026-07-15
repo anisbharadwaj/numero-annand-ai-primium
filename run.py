0c76fb8b6e53367c1ff696543c016e05623e2141
anisbharadwaj <anishbharadwaj685@gmail.com>
2026-07-15 08:53:20 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from app import create_app, db
+from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    print("🚀 Numero Annand AI Premium is starting on http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+if __name__ == '__main__':
+    print("🚀 Numero Annand AI Premium Booting Successfully...")
+    print("👉 Portal URL: http://127.0.0.1:5000")
+    print("🔒 Default Admin User: 'admin' | Password: 'AnnandSarmaAI2026'")
+    app.run(host='127.0.0.1', port=5000, debug=True)
