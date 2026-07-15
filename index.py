c8e3ddcb83cea5c61a68ae661e6a6fe790a20f4c
anisbharadwaj <anishbharadwaj685@gmail.com>
2026-07-15 10:19:34 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
+# Forces Vercel to dynamically recognize adjacent folder layouts for package modules
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import create_app
+
 app = create_app()
